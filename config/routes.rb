1eea69060848a9d646fadbc47e237ff44f5e89d3
odiseo <odiseo.b.c@gmail.com>
2023-02-25 19:16:20 -0600

    login and organization

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :organizations
-  devise_for :users
+
+  devise_for :users do
+    resources :organizations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
